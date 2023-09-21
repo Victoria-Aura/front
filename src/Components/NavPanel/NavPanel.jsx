8ca6b01a84321e1eda6cdb4bcf71478d2063a0d0
eNgeLzz <lakman0-0@mail.ru>
2023-09-22 00:26:01 +0700

    da

--- src/Components/NavPanel/NavPanel.jsx
+++ src/Components/NavPanel/NavPanel.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Navbar,Nav,Button } from 'react-bootstrap';
 import './NavPanel.css';
-import { LinkContainer } from 'react-router-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap';
+import { useGetImg } from '../../hooks/useGetImg';
+import { useGetAndSetData } from '../../hooks/useGetAndSetData';
+import ImgService from '../../API/ImgService';
+import { useState } from 'react';
 
 
 export const NavPanel = () => {
+//   const [dropdownState, setDropdownState] = useState({ open: false });
+
+//  const handleDropdownClick = () =>
+//  setDropdownState({ open: !dropdownState.open });
+  const [Dataimg, setimg, isLoading, Err] = useGetAndSetData(ImgService.getMainImg);
+    let avatar = useGetImg('avatar', Dataimg);
     return (
+
     <Navbar id='TopAppBar' collapseOnSelect expand="lg" className="nav visible" variant='dark' bg='dark'>
         <LinkContainer to="/" style={{marginLeft:"2%"}}>
           <Navbar.Brand href="#home" >VICTORY AURA</Navbar.Brand>
@@ -29,6 +40,7 @@ export const NavPanel = () => {
 
           </Nav>
           <Nav style={{paddingRight:"2%"}}>
+            
             <Button variant="secondary" id='logIn' className='logInBtn' >Авторизация</Button>
             <Button variant="danger" id='logIn' className='signInBtn' >Регистрация</Button>
           </Nav>
